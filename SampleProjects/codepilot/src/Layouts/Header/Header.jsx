091cdba95c76cbbe2c5d43b905e31ebb71f76cb7
WaioFaus <129754983+WaioFaus@users.noreply.github.com>
2024-11-02 17:56:55 +0700

    Styled the header
    I did it horribly but it's honest work

--- SampleProjects/codepilot/src/Layouts/Header/Header.jsx
+++ SampleProjects/codepilot/src/Layouts/Header/Header.jsx
@@ -1,25 +1,55 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
+import React, {useRef,useEffect,useState} from "react";
+import logo from './Logo.png';
+import userImg from './Rectangle.png'
 
 import "./Header.css";
 
 const Header = () => {
   const navigate = useNavigate();
+  const [selectedPage, setSelectedPage] = useState("Table");
+  // const logOut = () => {
+  //   localStorage.clear();
+  //   navigate("/");
+  // };
 
-  const logOut = () => {
-    localStorage.clear();
-    navigate("/");
-  };
   return (
-    <div className="admin-header">
-      <div className="admin-logo">Admin</div>
-      <button className="admin-head-logout-btn" onClick={logOut}>
-        LOGOUT
-        <FontAwesomeIcon icon={faRightToBracket} className="head-login-ico" />
-      </button>
+    <div className="header">
+      <div className="logo-and-nav">
+        <img src={logo} className="logo-image"  alt="LOGO" />
+        <div className="logo-text">
+          <h3>Morning coffee</h3>
+          <div className="function-selection">
+            <button
+              className={`nav-button ${selectedPage === "Table" ? "selected" : ""}`}
+              onClick={() => {
+                navigate("/admin/canvas");
+                setSelectedPage("Table");
+              }}
+            >
+              Table
+            </button>
+            <button
+              className={`nav-button ${selectedPage === "Order" ? "selected" : ""}`}
+              onClick={() => {
+                navigate("/admin/canvas");
+                setSelectedPage("Order");
+              }}
+            >
+              Order
+            </button>
+            <p>Saved</p>
+            <div
+              className="underline"
+              style={{ left: `${["Table", "Order"].indexOf(selectedPage) * 61}px` }}
+            ></div>
+          </div>
+        </div>
+      </div>
+      <img className="user-image" src={userImg} alt="Avatar" />
     </div>
   );
 };
-
 export default Header;
