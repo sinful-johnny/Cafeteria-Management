6019f9c045764564dc23e454ae31bb6bbcd69ae4
sinful_johnny <megaVNN321123@gmail.com>
2024-11-16 12:35:27 +0700

    build: drag & drop implementation

---
+++ SampleProjects/BlazorFE/BlazorApp2/wwwroot/dragDropInterop.js
@@ -0,0 +1,23 @@
+﻿window.dragDropInterop = {
+    setDragData: function (elementId, data) {
+        const draggable = document.getElementById(elementId);
+        draggable.addEventListener('dragstart', function (event) {
+            event.dataTransfer.setData("text/plain", data);
+        });
+    },
+
+    initializeDropzone: function (dropzoneId, dotNetHelper) {
+        const dropzone = document.getElementById(dropzoneId);
+
+        dropzone.addEventListener('dragover', function (event) {
+            event.preventDefault();
+        });
+
+        dropzone.addEventListener('drop', function (event) {
+            event.preventDefault();
+            //const data = JSON.parse(event.dataTransfer.getData("text/plain"));
+            const data = event.dataTransfer.getData("text/plain");
+            dotNetHelper.invokeMethodAsync('HandleDropData', data, event.clientX, event.clientY);
+        });
+    }
+};
